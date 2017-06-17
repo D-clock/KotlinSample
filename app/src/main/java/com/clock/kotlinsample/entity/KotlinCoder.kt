58b63f55a9991c090b46321bafdbe2533ebdff9d
Clock-YY <jixicai@yy.com>
2017-06-17 15:53:24 +0800

    refact repository

--- app/src/main/java/com/clock/kotlinsample/entity/KotlinCoder.kt
+++
@@ -1,7 +0,0 @@
-package com.clock.kotlinsample.entity
-
-/**
- * Created by Clock on 2017/6/11.
- */
-//使用data class声明会自动生成getters、 setters、 equals()、 hashCode()、 toString() 以及 copy()方法
-data class KotlinCoder(var name: String, var id: Int)
